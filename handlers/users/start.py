e55205e957efbf2c1450b2c5aa9ff5edfaa2076e
raxmatillo <milodcomposer@gmail.com>
2023-04-01 07:42:18 +0500

    prepare update excel hahha

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -12,17 +12,15 @@ from loader import dp, db, bot
 async def bot_start(message: types.Message):
     name = message.from_user.full_name
     # Foydalanuvchini bazaga qo'shamiz
-    try:
-        db.add_user(
-            full_name=name,
-            username=message.from_user.username,
-            telegram_id=message.from_user.id
-        )
-    except sqlite3.IntegrityError as err:
-        await bot.send_message(chat_id=ADMINS[0], text=err)
+    users = db.select_all_users()
+    if str(message.from_user.id) not in str(users):
+        try:
+            db.add_user(
+                full_name=name,
+                username=message.from_user.username,
+                telegram_id=message.from_user.id
+            )
+        except sqlite3.IntegrityError as err:
+            await bot.send_message(chat_id=ADMINS[0], text=err)
 
-    await message.answer("Xush kelibsiz!", reply_markup=menu)
-    # Adminga xabar beramiz
-    count = db.count_users()[0]
-    msg = f"{message.from_user.full_name} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
-    await bot.send_message(chat_id=ADMINS[0], text=msg)
+    await message.answer("Хуш келибсиз!", reply_markup=menu)
